503cea4f57dd5ddff9e98b2dcd414a4ccfd05ff5
Łukasz Grabowski <77535198+GrabowskiDev@users.noreply.github.com>
2023-12-01 22:13:34 +0100

    Add parallax background

--- player.gd
+++ player.gd
@@ -3,10 +3,11 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -850.0
 const JUMP_DEACCELERATION = 1.8
+const FALL_ANIM_DELAY = 100
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var anim = get_node("AnimationPlayer")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,15 +18,13 @@ func _physics_process(delta):
 			velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("jump"):
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	# Rotation animation
+	if velocity.y <= 0:
+		anim.play("Jump")
+	elif velocity.y > FALL_ANIM_DELAY:
+		anim.play("Fall")
 
 	move_and_slide() 
